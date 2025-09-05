826ce98faa84a6d60b6a19a7f2ed3f5c112a287e
sahilp-05 <sahilpat0001@gmail.com>
2025-09-04 21:59:17 -0400

    Fixed main.cpp to properly print

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main() {
     int a = 6;
     int b = 7;
 
-    cout << "a + b = " << MathUtils::add(a, b) << endl;
-    cout << "a * b = " << MathUtils::multiply(a, b) << endl;
-    cout << "a^2 = " << a << ": " << AdvancedMath::square(a) << endl;
-    cout << "b^2 = " << b << ": " << AdvancedMath::square(b) << endl;
+    std::cout << "a + b = " << MathUtils::add(a, b) << std::endl;
+    std::cout << "a * b = " << MathUtils::multiply(a, b) << std::endl;
+    std::cout << "a^2 = " << a << ": " << AdvancedMath::square(a) << std::endl;
+    std::cout << "b^2 = " << b << ": " << AdvancedMath::square(b) << std::endl;
 
     return 0;
 }
