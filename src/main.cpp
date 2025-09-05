62ba9ccc9190eaecc832876e144449f53fe18864
sahilp-05 <sahilpat0001@gmail.com>
2025-09-04 22:01:30 -0400

    Fixed numbers for a and b

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 #include "AdvancedMath.h"
 
 int main() {
-    int a = 6;
-    int b = 7;
+    int a = 3;
+    int b = 4;
 
     std::cout << "a + b = " << MathUtils::add(a, b) << std::endl;
     std::cout << "a * b = " << MathUtils::multiply(a, b) << std::endl;
