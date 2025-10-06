aae8eae1c90b717d529be0ffc573bd7a9457ed0e
sahilp-05 <sahilpat0001@gmail.com>
2025-10-05 20:02:50 -0400

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,8 @@ int main() {
 
     std::cout << "a + b = " << MathUtils::add(a, b) << std::endl;
     std::cout << "a * b = " << MathUtils::multiply(a, b) << std::endl;
-    std::cout << "a^2 = " << a << ": " << AdvancedMath::square(a) << std::endl;
-    std::cout << "b^2 = " << b << ": " << AdvancedMath::square(b) << std::endl;
+    std::cout << "square(a) = " << a << ": " << AdvancedMath::square(a) << std::endl;
+    std::cout << "square(b) = " << b << ": " << AdvancedMath::square(b) << std::endl;
 
     return 0;
 }
